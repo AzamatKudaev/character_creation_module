97d55288717479dbb6f2ab040dd18b81c19714b3
Azamat Kudaev <kudaevazamat@yandex.ru>
2022-12-20 20:39:40 +0300

    fixes input strings

--- main.py
+++ main.py
@@ -75,9 +75,9 @@ def choice_char_class() -> str:
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
-        char_class = input('Введи название персонажа, за которого хочешь',
-                           'играть: Воитель — warrior, Маг — mage, Лекарь',
-                           '— healer: ',
+        char_class = input('Введи название персонажа, за которого хочешь '
+                           'играть: Воитель — warrior, Маг — mage, Лекарь '
+                           '— healer: '
                            )
         if char_class == 'warrior':
             print('Воитель — дерзкий воин ближнего боя. Сильный, выносливый',
@@ -91,9 +91,9 @@ def choice_char_class() -> str:
             print('Лекарь — могущественный заклинатель. Черпает силы из',
                   'природы, веры и духов.',
                   )
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или',
-                               'любую другую кнопку, чтобы выбрать',
-                               'другого персонажа ',
+        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или '
+                               'любую другую кнопку, чтобы выбрать '
+                               'другого персонажа '
                                ).lower()
     return char_class
 
